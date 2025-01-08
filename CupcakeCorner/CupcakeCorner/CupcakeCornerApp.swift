8694a1c681eab247f5abdcb411e736866a2ccd9c
valenyang <valenyala@gmail.com>
2025-01-07 22:18:54 -0300

    50 - Cupcake Corner day 1

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Valentin Yang on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CupcakeCorner/CupcakeCorner/views/AddressView.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct AddressView: View {
+    var order: Order
+
+    var body: some View {
+        Form {
+        }
+    }
+}
