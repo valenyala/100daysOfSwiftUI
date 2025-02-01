ca50116a745ca7d97d88614bdc522673bb95bee1
valenyang <valenyala@gmail.com>
2025-01-31 23:26:43 -0300

    79 - HotProspects project creation

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Valentin Yang on 31/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selectedTab = "One"
+
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+
+            Text("Tab 2")
+                .tabItem {
+                    Label("Two", systemImage: "circle")
+                }
+                .tag("Two")
+        }
+    }
+}
+
+
+#Preview {
+    ContentView()
+}
---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Valentin Yang on 31/1/25.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
